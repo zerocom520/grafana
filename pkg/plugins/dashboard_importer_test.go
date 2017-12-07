9ca4cddf30243d38238f66348671c833d2f83f87
bergquist <carl.bergquist@gmail.com>
2017-12-07 18:05:11 +0100

    fixes failing tests

--- pkg/plugins/dashboard_importer_test.go
+++ pkg/plugins/dashboard_importer_test.go
@@ -91,7 +91,7 @@ func pluginScenario(desc string, t *testing.T, fn func()) {
 		setting.Cfg = ini.Empty()
 		sec, _ := setting.Cfg.NewSection("plugin.test-app")
 		sec.NewKey("path", "../../tests/test-app")
-		err := Init()
+		_, err := Init()
 
 		So(err, ShouldBeNil)
 
